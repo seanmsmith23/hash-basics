d807a460753190a92ad3735ce269d3e26bd24ca6
Sean Smith <seansmith1020@gmail.com>
2014-06-19 14:27:48 -0600

    fixed to interpolate

--- 05_print_address.rb
+++ 05_print_address.rb
@@ -2,4 +2,4 @@ require_relative "person"
 
 # Print Bjorn's address. It should read "444 Borg Lane, San Francisco, CA, 94104"
 
-print BJORN_BORG["address"]
+puts "#{BJORN_BORG["address"]["street"]}, #{BJORN_BORG["address"]["city"]}, #{BJORN_BORG["address"]["state"]}, #{BJORN_BORG["address"]["zip_code"]}"
