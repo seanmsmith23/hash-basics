fb878c94326106f9d06111625db56ba245c50dbc
Sean Smith <seansmith1020@gmail.com>
2014-06-19 14:25:35 -0600

    fixed to interpolate into string

--- 06_favorite_food.rb
+++ 06_favorite_food.rb
@@ -2,4 +2,5 @@ require_relative "person"
 
 # Print Bjorn's favorite foods. It should read "Bjorn's favorite foods are sushi, hamburgers, and mexican food."
 
-print BJORN_BORG["favorite_foods"]
+
+puts "Bjorn's favorite foods are #{BJORN_BORG["favorite_foods"][0]}, #{BJORN_BORG["favorite_foods"][1]}, and #{BJORN_BORG["favorite_foods"][2]}"
